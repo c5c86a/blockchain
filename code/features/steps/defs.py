52a3636c2efb32b5894406ceb49269cb5ab270e1
Nicos Maris <nicos.maris@gmail.com>
2017-12-11 00:34:51 +0200

    enable coverage of multiple processes

--- code/features/steps/defs.py
+++ code/features/steps/defs.py
@@ -1,5 +1,11 @@
 from aloe import step
 from requests import get
+from aloe import before
+import coverage
+
+@before.all
+def before_all():
+  coverage.process_startup()
 
 @step(r'user "([^"]*)" has hash "([^"]*)"')
 def hash(self, username, hashcode):
